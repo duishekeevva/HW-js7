c0817506d995df30ffb2731754ed4093d003e858
Elba <elba.duishekeeva16@gmail.com>
2023-11-02 21:11:43 +0600

    HW-js7

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1
+// function handleSum(arr) {
+//     let result = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         result+=arr[i]
+//     }
+//     return result
+// }
+// console.log(handleSum([1, 2, 3, 4, 5]));
+
+//2
+// function handleSum(arr) {
+//     let result = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         result += arr[i]
+//     }
+//     return result
+// }
+//
+// console.log(handleSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+//3
+// function findElement(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//        if (arr[i]===4) {
+//            return 'Есть!'
+//        }
+//     } return 'Нет!'
+// }
+//
+// console.log(findElement( [1, 2, 5, 9,4, 13, 10]));
+
